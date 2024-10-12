0546d2dc36753899c09e35e123a90b29d1a888c7
wudaochen <1452395258@qq.com>
2024-10-12 15:46:53 +0800

    删除图片

--- word2picture.py
+++ word2picture.py
@@ -1,17 +1,17 @@
 # encoding: UTF-8
-import time
-
-import requests
-from datetime import datetime
-from wsgiref.handlers import format_date_time
-from time import mktime
-import hashlib
 import base64
+import hashlib
 import hmac
-from urllib.parse import urlencode
 import json
+import requests
+import time
 from PIL import Image
+from datetime import datetime
 from io import BytesIO
+from time import mktime
+from urllib.parse import urlencode
+from wsgiref.handlers import format_date_time
+
 
 class AssembleHeaderException(Exception):
     def __init__(self, msg):
@@ -73,26 +73,27 @@ def assemble_ws_auth_url(requset_url, method="GET", api_key="", api_secret=""):
 
     return requset_url + "?" + urlencode(values)
 
+
 # 生成请求body体
-def getBody(appid,text):
-    body= {
+def getBody(appid, text):
+    body = {
         "header": {
             "app_id": appid,
-            "uid":"123456789"
+            "uid": "123456789"
         },
         "parameter": {
             "chat": {
                 "domain": "general",
-                "temperature":0.5,
-                "max_tokens":4096
+                "temperature": 0.5,
+                "max_tokens": 4096
             }
         },
         "payload": {
-            "message":{
-                "text":[
+            "message": {
+                "text": [
                     {
-                        "role":"user",
-                        "content":text
+                        "role": "user",
+                        "content": text
                     }
                 ]
             }
@@ -100,17 +101,19 @@ def getBody(appid,text):
     }
     return body
 
+
 # 发起请求并返回结果
-def main(text,appid,apikey,apisecret):
+def main(text, appid, apikey, apisecret):
     host = 'http://spark-api.cn-huabei-1.xf-yun.com/v2.1/tti'
-    url = assemble_ws_auth_url(host,method='POST',api_key=apikey,api_secret=apisecret)
-    content = getBody(appid,text)
+    url = assemble_ws_auth_url(host, method='POST', api_key=apikey, api_secret=apisecret)
+    content = getBody(appid, text)
     print(time.time())
-    response = requests.post(url,json=content,headers={'content-type': "application/json"}).text
+    response = requests.post(url, json=content, headers={'content-type': "application/json"}).text
     print(time.time())
     return response
 
-#将base64 的图片数据存在本地
+
+# 将base64 的图片数据存在本地
 def base64_to_image(base64_data, save_path):
     # 解码base64数据
     img_data = base64.b64decode(base64_data)
@@ -121,18 +124,16 @@ def base64_to_image(base64_data, save_path):
     # 保存图片到本地
     img.save(save_path)
 
+
 def gen_pic(desc):
+    # 解析并保存到指定位置
 
-# 解析并保存到指定位置
-
-
-
-    #运行前请配置以下鉴权三要素，获取途径：https://console.xfyun.cn/services/tti
-    APPID ='1579d1b5'
+    # 运行前请配置以下鉴权三要素，获取途径：https://console.xfyun.cn/services/tti
+    APPID = '1579d1b5'
     APISecret = 'ZDJlOWMzZDc3ZWNjM2U1YzUyOTVhOGJm'
     APIKEY = '4038eb2457f88e15b6ae1b5003069ea0'
     print(desc)
-    res = main(desc,appid=APPID,apikey=APIKEY,apisecret=APISecret)
+    res = main(desc, appid=APPID, apikey=APIKEY, apisecret=APISecret)
     data = json.loads(res)
     # print("data" + str(message))
     code = data['header']['code']
@@ -145,10 +146,7 @@ def gen_pic(desc):
         imageBase = imageContent["content"]
         filename = datetime.now().strftime("%Y%m%d%H%M%S") + ".png"
         savePath = f"static/image/{filename}"
-        base64_to_image(imageBase,savePath)
+        base64_to_image(imageBase, savePath)
         print("图片保存路径：" + savePath)
-    #保存到指定位置
+    # 保存到指定位置
     return savePath
-
-
-
